3de5b2c235956e0d80059609dc9e3e5534ae6396
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-18 15:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	let a=0;
+	let b=1;
+	if(num==1){
+		return a;
+	}
+	if(num==2){
+		return b;
+	}
+	let c;
+	for(int i=3;i<=num;i++){
+		c=a+b;
+		a=b;
+		b=c;
+		
+	}
+	return c;
 }
 
 module.exports = fibonacci;
